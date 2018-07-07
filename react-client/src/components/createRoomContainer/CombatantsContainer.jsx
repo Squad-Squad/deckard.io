3cf66723cb85bbde2a2ad275fde05a65bed0534a
Travis James Smith <trajamsmith@gmail.com>
2018-07-07 12:20:07 -0500

    Include avatars in the usersfornewroom list

--- react-client/src/components/createRoomContainer/CombatantsContainer.jsx
+++ react-client/src/components/createRoomContainer/CombatantsContainer.jsx
@@ -5,6 +5,9 @@ import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
 import FaceIcon from '@material-ui/icons/Face';
 import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import OtherProfileContainer from '../profileAndStats/OtherProfileContainer.jsx';
+import axios from 'axios';
 import { connect } from 'react-redux';
 import { removeUserFromNewRoom } from '../../../../redux/actions.js';
 
@@ -31,54 +34,140 @@ const styles = theme => ({
   chip: {
     margin: theme.spacing.unit,
   },
+  modal: {
+    position: 'absolute',
+    boxShadow: theme.shadows[5],
+  },
 });
 
-function ConnectedCombatantsContainer(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <Typography id="users-for-new-room-header">
-        Users &ensp;<span style={{ flex: "right" }}>{props.usersForNewRoom.length}/7</span>
-      </Typography>
+class ConnectedCombatantsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      userAvatarMap: [],
 
-      {/* logged in user's chip */}
-      <Chip
-        style={{ zIndex: '0' }}
-        avatar={
-          <Avatar>
-            <img src={props.avatarURL}
-              style={{
-                objectFit: 'cover',
-                borderRadius: '50%',
-                height: '32px',
-                width: '32px',
-              }} />
-          </Avatar>
-        }
-        label={props.loggedInUser}
-        className={classes.chip}
-      />
+      clickedFriend: '',
+    }
+  }
 
-      {/* invited users' chips */}
-      {
-        props.usersForNewRoom.slice(1).map((user, i) => {
-          return (
-            <Chip
-              avatar={
-                <Avatar>
-                  <FaceIcon />
-                </Avatar>
-              }
-              key={i}
-              label={user}
-              onDelete={props.removeUserFromNewRoom.bind(this, user)}
-              className={classes.chip}
-            />
-          )
-        })
-      }
-    </div>
-  );
+  componentWillReceiveProps() {
+    if (this.props.usersForNewRoom.slice(1)) {
+      Promise.all(this.props.usersForNewRoom.slice(1).map(friend => {
+        return axios.post('/api/userInfo', { user: friend });
+      }))
+        .then(res => {
+          const avatars = res.map(data => data.data.avatar);
+          const map = this.props.usersForNewRoom.slice(1).map((friend, i) => {
+            return [friend, avatars[i]];
+          });
+          this.setState({ userAvatarMap: map });
+        });
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      addFriend: true,
+    });
+  }
+
+  handleOpen(friend) {
+    console.log(friend);
+    this.setState({
+      clickedFriend: friend
+    }, () => {
+      this.setState({
+        open: true,
+      });
+    });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return ([
+      <div>
+        <Typography id="users-for-new-room-header">
+          Users &ensp;<span style={{ flex: "right" }}>{this.props.usersForNewRoom.length}/7</span>
+        </Typography>
+
+        {/* logged in user's chip */}
+        <Chip
+          style={{ zIndex: '0' }}
+          avatar={
+            <Avatar>
+              <img src={this.props.avatarURL}
+                style={{
+                  objectFit: 'cover',
+                  borderRadius: '50%',
+                  height: '32px',
+                  width: '32px',
+                }} />
+            </Avatar>
+          }
+          label={this.props.loggedInUser}
+          className={classes.chip}
+        />
+
+        {/* invited users' chips */}
+        {this.state.userAvatarMap.map(userAvatar => {
+          console.log('USERAVATAR', userAvatar)
+          if (userAvatar[1] &&
+            userAvatar[1] !== './assets/roboheadwhite.png') {
+            return (
+              <Chip
+                style={{ zIndex: '0' }}
+                avatar={
+                  <Avatar>
+                    <img src={userAvatar[1]}
+                      style={{
+                        objectFit: 'cover',
+                        borderRadius: '50%',
+                        height: '32px',
+                        width: '32px',
+                      }} />
+                  </Avatar>
+                }
+                label={userAvatar[0]}
+                className={classes.chip}
+              />
+            )
+          } else {
+            return (
+              <Chip
+                style={{ zIndex: '0' }}
+                label={userAvatar[0]}
+                className={classes.chip}
+              />
+            )
+          }
+        })}
+
+
+      </div>,
+      <Modal
+        style={{ alignItems: 'center', justifyContent: 'center' }}
+        open={this.state.open}
+        onClose={this.handleClose.bind(this)}
+      >
+        <div style={{
+          top: '20%',
+          margin: 'auto',
+          width: '700px',
+          backgroundColor: 'black',
+        }}
+          className={classes.modal}>
+          <OtherProfileContainer friend={this.state.clickedFriend} />
+        </div>
+      </Modal>
+    ]);
+  }
+
 }
 
 ConnectedCombatantsContainer.propTypes = {
