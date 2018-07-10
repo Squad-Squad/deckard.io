fb0a33d748a5c93049c93454c3d0b03cb0ecb8df
Travis James Smith <trajamsmith@gmail.com>
2018-07-10 11:48:17 -0500

    Remove splash page cruft

--- react-client/src/components/Splash.jsx
+++ react-client/src/components/Splash.jsx
@@ -9,9 +9,8 @@ function mapStateToProps(state) {
 
 class Splash extends Component {
   render() {
-    const svg = '<svg viewBox="0 0 800 600">< symbol id = "s-text" ><text text-anchor="middle"x="50%"y="35%"class="text--line">Elastic</text><text text-anchor="middle"x="50%"y="68%"class="text--line2">Stroke</text></symbol ><g class="g-ants"><use xlink: href="#s-text"class="text-copy"></use><use xlink: href="#s-text"class="text-copy"></use><use xlink: href="#s-text"class="text-copy"></use><use xlink: href="#s-text"class="text-copy"></use><use xlink: href="#s-text"class="text-copy"></use></g ></svg > ';
     return (
-      <div dangerouslySetInnerHTML={{ __html: svg }}>
+      <div>
       </div>
     );
   }
