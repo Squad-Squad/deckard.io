9dc8eb3ed94e3a8034717f50ee6c479119255eb1
Mitchell Stewart <mitchell.stewart08@gmail.com>
2018-07-13 15:09:20 -0500

    Add basic rules and about copy

--- react-client/src/components/AboutDialogue.jsx
+++ react-client/src/components/AboutDialogue.jsx
@@ -8,7 +8,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 
 class AboutDialogue extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       open: false,
       scroll: 'paper',
@@ -39,16 +39,40 @@ class AboutDialogue extends React.Component {
         <DialogTitle id="scroll-dialog-title">Rules/About</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Deckard IO is a game imagined in the vain of Alan Turing's canonical Turing Test.
+            <strong>About</strong>
+            <p>
+              Deckard.io is a game inspired by Alan Turing's canonical{' '}
+              <a href="https://en.wikipedia.org/wiki/Turing_test">Turing Test</a> but with a unique
+              spin, instead of just a computer trying to fool a human, in Deckard.io, you are a
+              human trying to fool other humans into thinking you are a computer. We call this The
+              Reverse Turing Test. The name is inspired by the famous cyberpunk movie{' '}
+              <a href="https://www.imdb.com/title/tt0083658/">Blade Runner</a>. It is an open source
+              application created by <a href="https://github.com/Squad-Squad">Squad Squad</a>
+            </p>
+            <br />
+            <strong>Rules</strong>
+            <p>
+              Objectives:
+              <ul>
+                <li>
+                  1. Guess who the actual chatbot is in the chatroom. You get points if you guess
+                  correctly
+                </li>
+                <li>
+                  2. Have the other humans in the chatroom guess that you are the A.I. Chatbot
+                </li>
+              </ul>
+            </p>
 
-            Players are thrown in an anonymous chatroom together.
-            One player is a bot, the rest Hyumons. Hyumons get points whent the get the others
-            to guess that they are a bot and when they guess which player is the bot.
-            </DialogContentText>
+            {/* Deckard IO is a game imagined in the vain of Alan Turing's canonical Turing Test.
+            Players are thrown in an anonymous chatroom together. One player is a bot, the rest
+            Hyumons. Hyumons get points whent the get the others to guess that they are a bot and
+            when they guess which player is the bot. */}
+          </DialogContentText>
         </DialogContent>
         <Button onClick={this.handleClose} color="primary">
           Close
-            </Button>
+        </Button>
       </Dialog>
     );
   }
